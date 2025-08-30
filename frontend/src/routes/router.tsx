06e73738aa561b6028920cf1bcf8a6e6a22cd06b
eruzetaien <muhammad.ruzain@ui.ac.id>
2025-08-30 19:56:09 +0700

    chore(frontend): delete built-in App.tsx

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,12 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import LoginPage from "../pages//LoginPage";
 import ProfilePage from "../pages/ProfilPage";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
+        element: <LoginPage />,
     },
     {
         path: "/login",
