0cedab5c8edb36baf2fdadce41b17f5e30b91346
eruzetaien <muhammad.ruzain@ui.ac.id>
2025-10-18 10:47:42 +0700

    fix(frontend): redirect to profile page if users access login page and have already logged in

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useProfileQuery } from "../hooks/useUserProfile";
+import { useEffect } from "react";
 
 function LoginPage() {
   const loginWithGoogle = () => {
     window.location.href = `${import.meta.env.VITE_AUTH_BASE_URL}/login/google`;
   };
+  const { data: user, isLoading} = useProfileQuery();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user) {
+      navigate("/profile");
+    }
+  }, [user]);
+
+  if (isLoading) return <p>Loading...</p>;
 
   return (
     <button onClick={() => loginWithGoogle()}>
