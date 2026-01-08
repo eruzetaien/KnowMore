796ea4283b9fd41bb2d6447bcfbbce1f3a5439b0
eruzetaien <muhammadruzain10@gmail.com>
2026-01-08 20:30:42 +0700

    design: add toast feedback for profile updates

--- frontend/src/hooks/useUserProfile.ts
+++ frontend/src/hooks/useUserProfile.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { fetchUserProfile, updateProfile } from "../api/userApi";
 import type { UpdateProfileRequest, UserResponse } from "../types/userType";
+import toast from "react-hot-toast";
 
 export const useProfileQuery = () => {
   return useQuery<UserResponse>({
@@ -12,5 +13,21 @@ export const useProfileQuery = () => {
 export const useUpdateProfile = () => {
   return useMutation<UserResponse, Error, UpdateProfileRequest>({
     mutationFn: updateProfile,
+
+    onMutate: () => {
+      toast.loading("Updating profile...", { id: "update-profile" });
+    },
+
+    onSuccess: () => {
+      toast.success("Profile updated successfully!", {
+        id: "update-profile",
+      });
+    },
+
+    onError: (error) => {
+      toast.error(error.message || "Failed to update profile", {
+        id: "update-profile",
+      });
+    },
   });
 };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ToastContainer } from 'react-toastify'
+import { Toaster } from 'react-hot-toast';
 import { GameHubProvider } from './context/GameHubContext.tsx'
 import Background from './components/Background.tsx'
 
@@ -16,7 +16,7 @@ createRoot(document.getElementById('root')!).render(
       <GameHubProvider>
         <Background>
           <RouterProvider router={router} />
-          <ToastContainer />
+          <Toaster />
         </Background>
       </GameHubProvider>
     </QueryClientProvider>
