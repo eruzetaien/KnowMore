806a00d7edcb9b93469b23733782f0655fe1bfed
eruzetaien <muhammadruzain10@gmail.com>
2026-01-09 20:50:49 +0700

    design: add toast feedback for create fact and fact group

--- frontend/src/hooks/useFact.ts
+++ frontend/src/hooks/useFact.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import type { CreateFactGroupRequest, CreateFactRequest, FactGroupResponse, FactResponse} from "../types/factType";
 import { createFact, createFactGroup, fetchAllFactGroup } from "../api/factApi";
+import toast from "react-hot-toast";
 
 export const useAllUserFactQuery = () => {
   return useQuery<FactGroupResponse[]>({
@@ -12,12 +13,43 @@ export const useAllUserFactQuery = () => {
 export const useCreateFactGroup = () => {
   return useMutation<FactGroupResponse, Error, CreateFactGroupRequest>({
     mutationFn: createFactGroup,
+
+    onMutate: () => {
+      toast.loading("Creating fact group...", { id: "create-fact-group" });
+    },
+
+    onSuccess: () => {
+      toast.success("Fact group created successfully!", {
+        id: "create-fact-group",
+      });
+    },
+
+    onError: (error) => {
+      toast.error(error.message || "Failed to create fact group", {
+        id: "create-fact-group",
+      });
+    },
   });
 };
 
 export const useCreateFact = () => {
   return useMutation<FactResponse, Error, CreateFactRequest>({
     mutationFn: createFact,
+
+    onMutate: () => {
+      toast.loading("Creating fact...", { id: "create-fact" });
+    },
+
+    onSuccess: () => {
+      toast.success("Fact created successfully!", {
+        id: "create-fact",
+      });
+    },
+
+    onError: (error) => {
+      toast.error(error.message || "Failed to created fact", {
+        id: "create-fact",
+      });
+    },
   });
 };
-
