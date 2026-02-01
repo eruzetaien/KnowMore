5dc5cd1564d61e31171771dc39c5baa225f2af94
eruzetaien <muhammadruzain10@gmail.com>
2026-02-01 08:33:04 +0700

    design: implement fact selection in preparation phase

--- frontend/src/components/PreparationPhase.tsx
+++ frontend/src/components/PreparationPhase.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useGameHub } from "../context/GameHubContext";
 import { useCountdown } from "../hooks/useCountdown";
 
@@ -6,29 +6,50 @@ import player1Thinking from "../assets/players/state/player1-thinking.png";
 import player2Thinking from "../assets/players/state/player2-thinking.png";
 import player1Chilling from "../assets/players/state/player1-chilling.png";
 import player2Chilling from "../assets/players/state/player2-chilling.png";
+import paperContent from "../assets/paper-content.svg";
+import paperTableOfContent from "../assets/paper-table-of-content.svg";
+import arrowBackIcon from "../assets/icons/back-arrow.svg";
+import sendButton from "../assets/buttons/send-button.svg";
+
 import PlayerState from "./PlayerState";
 import CouponCard from "./CouponCard";
+import type { FactForGame } from "../types/factType";
 
+type PaperId = "table-of-content" | "content";
 
 export default function PreparationPhase() {
   const { allPlayerData, isLoading: hubLoading, sendStatements, room, preparationPhaseData } = useGameHub();
 
-  const [selectedFacts, setSelectedFacts] = useState<string[]>([]);
+  const factGroups = preparationPhaseData.playerFacts;
+  const [activeGroupId, setActiveGroupId] = useState<string | null>(null);
+  const activeGroup = useMemo(
+      () => factGroups.find(g => g.id === activeGroupId) ?? null,
+      [factGroups, activeGroupId]
+    );
+  const [isFactOpen, setIsFactOpen] =  useState<boolean>(false);
+
+  const [selectedFacts, setSelectedFacts] = useState<FactForGame[]>([]);
   const [lie, setLie] = useState("");
+
+  const [isStatementSent, setIsStatementSend] = useState<boolean>(false);
 
   const { minutes, seconds } = useCountdown(180); // 3 minutes
 
   if (hubLoading) return <p>Loading hub connection...</p>;
 
-  const handleFactSelect = (factId: string) => {
+  const handleFactSelect = (fact: FactForGame) => {
     setSelectedFacts((prev) => {
-      if (prev.includes(factId)) {
-        return prev.filter((id) => id !== factId);
+      if (prev.some(f => f?.id === fact.id)) {
+        return prev.filter(f => f?.id !== fact.id);
       }
-      if (prev.length < 2) {
-        return [...prev, factId];
+
+      const updated = [...prev, fact];
+
+      if (updated.length > 2) {
+        return updated.slice(updated.length - 2);
       }
-      return prev; // ignore if already 2 selected
+
+      return updated;
     });
   };
 
@@ -37,10 +58,29 @@ export default function PreparationPhase() {
       alert("Please select exactly 2 facts and enter a lie.");
       return;
     }
-    await sendStatements(room.code, lie, selectedFacts[0], selectedFacts[1]);
-    setSelectedFacts([]);
-    setLie("");
+    await sendStatements(room.code, lie, selectedFacts[0].id, selectedFacts[1].id);
+    setIsStatementSend(true);
   };
+
+  const [order, setOrder] = useState<[PaperId, PaperId]>([
+      "table-of-content",
+      "content",
+    ]);
+  const [animating, setAnimating] = useState(false);
+  const bringToTop = (paper: PaperId) => {
+    if (animating || order[0] === paper) return;
+
+    setAnimating(true);
+
+    setTimeout(() => {
+      setOrder((prev) =>
+        prev[0] === paper ? prev : [paper, prev[0]]
+      );
+      setAnimating(false);
+    }, 300);
+  };
+
+  const isTop = (card: PaperId) => order[0] === card;
 
   return (
     <div className="flex flex-col justify-center items-center">
@@ -50,9 +90,7 @@ export default function PreparationPhase() {
         <h2 className="text-5xl" >{minutes}:{seconds.toString().padStart(2, "0")}</h2>
       </div>
 
-
       <h2 className="text-5xl mb-4">Prepare your card to play!</h2>
-
 
       {/* Player State */}
       <div className="flex justify-between w-11/12">
@@ -66,15 +104,56 @@ export default function PreparationPhase() {
         <div className="flex flex-col w-full items-center justify-center">
           <div className="w-4/5 bg-platinum rounded-3xl border-4 border-heathered-grey p-6 outline-2 -outline-offset-7 outline-heathered-grey">
             <div className="flex flex-col gap-y-2">
-              <CouponCard>
-                  <div>{selectedFacts[0]}</div>
-              </CouponCard>
-              <CouponCard>
-                  <div>{selectedFacts[1]}</div>
-              </CouponCard>
+
+              <div
+                onClick={() => {
+                  if (!isStatementSent) {
+                    setIsFactOpen(true);
+                  }
+                }}
+                className={`cursor-pointer transition-transform ${
+                  !isStatementSent ? "hover:scale-101" : "opacity-50 cursor-not-allowed"
+                }`}
+              >
+                <CouponCard>
+                  <div style={{ opacity: selectedFacts?.[0] ? 1 : 0.5 }}>
+                    {selectedFacts?.[0]?.description || "Pick your fact to play"}
+                  </div>
+                </CouponCard>
+              </div>
+
+              <div
+                onClick={() => {
+                  if (!isStatementSent) {
+                    setIsFactOpen(true);
+                  }
+                }}
+                className={`cursor-pointer transition-transform ${
+                  !isStatementSent ? "hover:scale-101" : "opacity-50 cursor-not-allowed"
+                }`}
+              >
+                <CouponCard>
+                  <div style={{ opacity: selectedFacts?.[1] ? 1 : 0.5 }}>
+                    {selectedFacts?.[1]?.description || "Pick your fact to play"}
+                  </div>
+                </CouponCard>
+              </div>
+
               <CouponCard isRed={true}>
-                  <div>{lie}</div>
+                <textarea
+                  value={lie}
+                  onChange={(e) => {
+                    setLie(e.target.value);
+                    e.target.style.height = "auto";
+                    e.target.style.height = `${e.target.scrollHeight}px`;
+                  }}
+                  placeholder="Write your lie"
+                  className="w-full resize-none overflow-hidden bg-transparent border-0 outline-none focus:outline-none text-inherit placeholder:opacity-70"
+                  disabled={isStatementSent}
+                />
               </CouponCard>
+
+              
             </div>
           </div>
         </div>
@@ -88,54 +167,152 @@ export default function PreparationPhase() {
         />
       </div>
 
-      {preparationPhaseData.playerFacts?.length ? (
-        <ul className="space-y-2 mt-2">
-          {preparationPhaseData.playerFacts.map((group) => (
-            <li key={group.id} className="p-3 bg-gray-700 rounded-lg shadow">
-              <p className="font-medium">{group.name}</p>
-              <ul className="ml-4 mt-1 list-disc text-sm">
-                {group.facts.map((fact) => (
-                  <li key={fact.id}>
-                    <label className="flex items-center gap-2">
-                      <input
-                        type="checkbox"
-                        checked={selectedFacts.includes(fact.id)}
-                        onChange={() => handleFactSelect(fact.id)}
-                        disabled={
-                          !selectedFacts.includes(fact.id) && selectedFacts.length >= 2
-                        }
-                      />
-                      {fact.description}
-                    </label>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p className="text-gray-400">No fact groups found.</p>
-      )}
-
-      {/* Lie input */}
-      <div className="mt-4">
-        <label className="block font-medium mb-2">Your Lie</label>
-        <input
-          type="text"
-          value={lie}
-          onChange={(e) => setLie(e.target.value)}
-          className="w-full p-2 rounded bg-gray-800 border border-gray-600"
-          placeholder="Enter your lie here"
-        />
+      <div className="mt-10">
+        {isStatementSent ? (
+          <p className="text-3xl">
+            Waiting opponent's decision ...
+          </p>
+        ) : (
+          <button
+            onClick={handleSubmit}
+            className="cursor-pointer hover:scale-105"
+          >
+            <img src={sendButton} alt="send button" />
+          </button>
+        )}
       </div>
 
-      {/* Submit button */}
-      <button
-        onClick={handleSubmit}
-        className="mt-4 px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded text-white"
-      >
-        Submit Statements
-      </button>
+      {isFactOpen && (
+        <div className="fixed inset-0 z-50 w-screen h-screen bg-black/50  backdrop-blur-xs" onClick={() => setIsFactOpen(false)}>
+          <div className="flex justify-center overflow-clip" onClick={(e) => e.stopPropagation()}>
+            {/* Table of Content*/}
+            <div
+              className={`
+              Paper Card 2
+                absolute h-full flex justify-center items-center overflow-auto 
+                transition-all duration-300 ease-out
+                ${
+                  isTop("table-of-content")
+                    ? "z-20 -translate-x-8 rotate-1 -bottom-18 " //
+                    : animating
+                    ? "-translate-x-16 -rotate-3 -bottom-12"
+                    : "z-10 -translate-x-12 -rotate-2 -bottom-20"
+                }
+              `}
+            >
+              <div className="relative h-full flex justify-center items-center ">
+                <img className="h-full" src={paperTableOfContent} alt=""/>
+
+                <div className="absolute w-full h-full flex justify-center p-20">
+                  <div className="w-full max-w-3xl">
+                    <h1 className="text-4xl text-center">
+                      Table of Contents
+                    </h1>
+
+                    {factGroups?.length ? (
+                      <ul className="space-y-4 text-xl">
+                        {factGroups.map((group, index) => (
+                          <li
+                            key={group.id}
+                            className="flex gap-1 cursor-pointer hover:font-bold"
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              setActiveGroupId(group.id);
+                              bringToTop("content");
+                            }}
+                          >
+                            <span>
+                              {index + 1}. {group.name}
+                            </span>
+
+                            <span className="flex-1 overflow-hidden whitespace-nowrap text-2xl leading-none">
+                              ................................................................................................
+                            </span>
+
+                            <span className="text-gray-500">›</span>
+                          </li>
+                        ))}
+                      </ul>
+                    ) : (
+                      <p className="text-center text-xl text-gray-500">
+                        No content available.
+                      </p>
+                    )}
+
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+
+            {/* Content */}
+            <div
+              className={`
+              Paper Card 2
+                absolute h-full -bottom-4 flex justify-center items-center overflow-auto  
+                transition-all duration-300 ease-out
+                ${
+                  isTop("content")
+                    ? "z-20 -rotate-1"
+                    : animating
+                    ? "translate-x-16 rotate-3"
+                    : "z-10 translate-x-6 rotate-2"
+                }
+              `}
+            >
+              <div className="relative h-full flex justify-center items-center ">
+                <img className="h-full" src={paperContent} alt=""/>
+
+                <div className="absolute w-full h-full flex flex-col pl-20 pr-28 py-16 overflow-auto">
+
+                  <div className="flex justify-between">
+                    <button 
+                      className="cursor-pointer opacity-70 hover:opacity-100 transition-opacity" 
+                      onClick={() => { 
+                        bringToTop("table-of-content"); 
+                      }}
+                    >
+                      <img className="h-[36px]" src={arrowBackIcon} alt="" />
+                    </button>
+                  </div>
+                  
+                  <h1 className= "w-full text-4xl text-center mb-10 mt-5">
+                    {activeGroup ? activeGroup.name : "Content"}
+                  </h1>
+
+                  {activeGroup ? (
+                    <ul className="space-y-4 text-xl px-10">
+                      
+                      {activeGroup.facts.map((fact) => (
+                        <li key={fact.id} className="pb-2]">
+                          <span
+                            className={`text-2xl cursor-pointer ${
+                              selectedFacts.some((f) => f.id === fact.id) ? "highlight" : ""
+                            }`}
+                            onClick={() => {
+                              handleFactSelect(fact);
+                            }}
+                          >
+                            {fact.description}
+                          </span>
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p className="text-gray-500">
+                      Select a group from the Table of Contents
+                    </p>
+                  )}
+                </div>
+
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      )}
+      
     </div>
   );
 }
